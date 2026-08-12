e275b2c2f3182d8494c788f28eda5bd6603770a4
paras-2817 <paras.2503604106@vcet.edu.in>
2026-08-12 09:10:31 +0530

    Add files via upload

---
+++ BST.java
@@ -0,0 +1,106 @@
+class BST {
+
+    // Node of the BST
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    Node root;
+
+    // Insert a node
+    Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        }
+
+        if (data < root.data) {
+            root.left = insert(root.left, data);
+        } else if (data > root.data) {
+            root.right = insert(root.right, data);
+        }
+
+        return root;
+    }
+
+    // Inorder Traversal: Left -> Root -> Right
+    void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
+    // Preorder Traversal: Root -> Left -> Right
+    void preorder(Node root) {
+        if (root != null) {
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    // Postorder Traversal: Left -> Right -> Root
+    void postorder(Node root) {
+        if (root != null) {
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+
+    // Search for a value in BST
+    boolean search(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == key) {
+            return true;
+        }
+
+        if (key < root.data) {
+            return search(root.left, key);
+        } else {
+            return search(root.right, key);
+        }
+    }
+
+    public static void main(String[] args) {
+        BST tree = new BST();
+
+        // Insert elements
+        tree.root = tree.insert(tree.root, 50);
+        tree.root = tree.insert(tree.root, 30);
+        tree.root = tree.insert(tree.root, 70);
+        tree.root = tree.insert(tree.root, 20);
+        tree.root = tree.insert(tree.root, 40);
+        tree.root = tree.insert(tree.root, 60);
+        tree.root = tree.insert(tree.root, 80);
+
+        // Traversals
+        System.out.print("Inorder: ");
+        tree.inorder(tree.root);
+
+        System.out.print("\nPreorder: ");
+        tree.preorder(tree.root);
+
+        System.out.print("\nPostorder: ");
+        tree.postorder(tree.root);
+
+        // Search
+        int key = 40;
+
+        if (tree.search(tree.root, key)) {
+            System.out.println("\n" + key + " found in BST");
+        } else {
+            System.out.println("\n" + key + " not found in BST");
+        }
+    }
+}
